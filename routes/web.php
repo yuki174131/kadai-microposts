878abbd2c921b1ea9956e434fb0b2f7400f96784
yuki.sekine <yuki31.messi10@gmail.com>
2019-04-01 12:27:55 +0000

    web.phpファイル修正

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'MicropostsControllor@index');
+Route::get('/', 'MicropostsController@index');
 
 //ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
