2a4ee7ed74f7ee92fa14f4e941a3a3e385796940
yuki.sekine <yuki31.messi10@gmail.com>
2019-04-02 09:11:11 +0000

    microposts.blade.phpのみエラー部修正

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -1,7 +1,7 @@
 <ul class="media-list">
     @foreach ($microposts as $micropost)
         <li class="media mb-3">
-            <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->$user->email, 50) }}" alt="">
+            <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->user->email, 50) }}" alt="">
             <div class="media-body">
                 <div>
                     {!! link_to_route('users.show', $micropost->user->name, ['id' => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
